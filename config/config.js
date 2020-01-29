36ffbecfe252d722eb9c87d37e8205f849912a87
luke01455 <luke0145555@hotmail.co.uk>
2020-01-29 16:23:41 +0000

    removed config form gitignore for now

---
+++ config/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://luke01455:txz4fN4EV2gmscdV@quizappcluster-exfyt.mongodb.net/test?retryWrites=true&w=majority',
+    SECRET_KEY: 'fdsfdsffdsf4234234'
+}
